8f4d10f70f0d5c8ee0e36c758f0999473cf70e4f
David Taing <adavidtaing@gmail.com>
2021-10-12 01:07:46 +1100

    Finished Module 4

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const express = require("express");
 
 const app = express();
 
+const { accounts, users, writeJSON } = require('./data');
+
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 
@@ -11,10 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 
-const accountData = fs.readFileSync("src/json/accounts.json", { encoding: "utf8" });
-const accounts = JSON.parse(accountData);
-const userData = fs.readFileSync("src/json/users.json", {encoding: "utf8"})
-const users = JSON.parse(userData);
+
 
 app.get("/", (req, res) => {
   res.render("index", { title: "Account Summary", accounts: accounts });
@@ -28,8 +27,7 @@ app.post("/transfer", (req, res) => {
   accounts[req.body.from].balance -= req.body.amount;
   accounts[req.body.to].balance += parseInt(req.body.amount);
 
-  const accountsJSON = JSON.stringify(accounts);
-  fs.writeFileSync(path.join(__dirname, "/json/accounts.json"), accountsJSON, "utf8");
+  writeJSON();
   res.render("transfer", { message: "Transfer Completed" });
 });
 
@@ -41,8 +39,7 @@ app.post("/payment", (req, res) => {
   accounts.credit.balance -= req.body.amount;
   accounts.credit.available = parseInt(accounts.credit.available) + parseInt(req.body.amount);
 
-  const accountsJSON = JSON.stringify(accounts);
-  fs.writeFileSync(path.join(__dirname, "/json/accounts.json"), accountsJSON, "utf8");
+  writeJSON();
   res.render("payment", { message: "Payment Successful", account: accounts.credit });
 });
 
